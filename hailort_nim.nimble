207ed2eebc8484bc9a062bd133e2e828a2d588f0
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2026-05-01 15:19:46 +0900

    Bump version to 0.2.0
    Mark the async vstream and async detector additions as the next minor release.
    
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- hailort_nim.nimble
+++ hailort_nim.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "0.1.0"
+version       = "0.2.0"
 author        = "Takeyoshi Kikuchi"
 description   = "Unofficial Nim bindings for HailoRT"
 license       = "MIT"
--- src/hailort_nim.nim
+++ src/hailort_nim.nim
@@ -1,12 +1,13 @@
 import ./hailort_nim/lowlevel
-import ./hailort_nim/highlevel/async_vstream_runner
 import ./hailort_nim/highlevel/detector
-import ./hailort_nim/highlevel/async_detector
 import ./hailort_nim/highlevel/device_stats
 import ./hailort_nim/highlevel/runtime_device_stats
 import ./hailort_nim/models/detection
-
 export lowlevel except okVoid, makeError
-export async_vstream_runner
-export detector, detection, async_detector
+export detector, detection
 export device_stats, runtime_device_stats
+when defined(hailortAsyncVstream):
+  import ./hailort_nim/highlevel/async_vstream_runner
+  import ./hailort_nim/highlevel/async_detector
+  export async_vstream_runner
+  export async_detector
